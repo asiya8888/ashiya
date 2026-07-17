090ae8c65b1783d34c3962625598c6c29c1087b4
apple <apple@MacBookAir.lan>
2026-07-17 14:49:31 +0500

    Redesign kitchen scene

--- src/components/rooms/KitchenScene.tsx
+++ src/components/rooms/KitchenScene.tsx
@@ -4,22 +4,38 @@ export function KitchenScene() {
   return (
     <>
       <p className="room-title">Kitchen</p>
+      <div className="kitchen-glow" aria-hidden="true" />
       <div className="kitchen-window">
+        <span className="window-crossbar window-crossbar-vertical" />
+        <span className="window-crossbar window-crossbar-horizontal" />
+        <span className="window-frost" />
         {Array.from({ length: 18 }, (_, index) => <span className="snowflake" key={index} style={snowStyle(index + 40)} />)}
       </div>
       <div className="kitchen-cabinets">
-        <span className="cabinet-row" />
+        <span className="cabinet-door cabinet-door-left" />
+        <span className="cabinet-door cabinet-door-mid" />
+        <span className="cabinet-door cabinet-door-right" />
+        <span className="cabinet-shelf" />
       </div>
       <div className="kitchen-counter">
-        <span className="sink" />
-        <span className="stove" />
+        <span className="stove"><span /></span>
+        <span className="sink"><span /></span>
+        <span className="faucet" />
         <span className="small-jars" />
       </div>
-      <div className="fridge" />
+      <div className="fridge">
+        <span className="fridge-freezer" />
+        <span className="fridge-handle fridge-handle-top" />
+        <span className="fridge-handle fridge-handle-bottom" />
+        <span className="fridge-note" />
+      </div>
       <div className="table">
-        <span className="chair chair-left" />
-        <span className="chair chair-right" />
-        <span className="chair chair-back" />
+        <span className="table-top" />
+        <span className="table-leg table-leg-left" />
+        <span className="table-leg table-leg-right" />
+        <span className="chair chair-left"><span /></span>
+        <span className="chair chair-right"><span /></span>
+        <span className="chair chair-back"><span /></span>
         <span className="mug" />
       </div>
       <span className="doorway doorway-right" aria-hidden="true" />
